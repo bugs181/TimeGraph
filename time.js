98e355790aa648fac7d90368054b0c94bf210109
Levi Roberts <bugs181@gmail.com>
2019-01-02 20:54:05 -0600

    More improvements

--- time.js
+++ time.js
@@ -10,6 +10,7 @@
     'use strict'
 
     var gun = this, root = gun.back(-1)
+    var ify = Gun.node.ify
     var opts = gun && gun._ && gun._.root && gun._.root.opt
     var enforceData = opts.enforceData
 
@@ -47,12 +48,25 @@
         }
 
         // Create new data/soul for every .put to match original API
+
+        // Closest working, but loses context
         var soul = (gun.back('opt.uuid') || Gun.text.random)(9)
-        data._ = { '#': soul }
-        return timeMethods.put.call(this, data, cb, null, null, soul)
+        //return timeMethods.put.call(this, ify(data, soul), soul, null, soul)
+
+        //this._.dub = soul
+        //return timeMethods.put.call(this, ify(data, gun._.dub), null, null, soul)
+
+        //this._.dub = soul
+        //var as = this
+        //as._.dub = soul
+        //return timeMethods.put.call(this, ify(data, soul), cb, as)
+
+        //timeMethods.put.call(this, data, cb, this, null, soul)
+        timeMethods.put.call(this, ify(data, soul), cb, this, null, soul)
+        return this
       },
 
-      put: function(data, cb, as, rData, rSoul) {
+      put: function(data, cb, as, rSoul) {
         var gunCtx = this
 
         if (Gun.is(data)) {
@@ -60,92 +74,89 @@
             if (!soul)
               return cb && cb({ err: "Timegraph cannot link `undefined`!" })
 
-            timeMethods.put.call(gunCtx, null, cb, as, data, soul)
+            timeMethods.put.call(gunCtx, Gun.val.link.ify(soul), cb, as, soul)
           }, true)
           return this
         }
 
         gun.get(function(soul) {
-          if (rSoul)
-            soul = rSoul
-
-          if (soul) {
-            //var ify = Gun.node.ify
-            function ify(obj, str) {
-              return { _: { '#': str } }
+          // This fixes .set(), because passing soul to gun.put() creates weird behavior.
+          if (!soul) {
+            if (!gun._.dub) {
+              gun._.dub = (gun.back('opt.uuid') || Gun.text.random)(9)
+              soul = gun._.dub
             }
-
-            var t = new Date(Gun.state()).toISOString().split(/[\-t\:\.z]/ig)
-            var rid = 'timepoint/' + soul
-            t = [rid].concat(t)
-
-            // Working example of original modified slightly
-            var milli = ify({}, t.join(':'))
-            milli['soul'] = soul
-
-            var tmp = t.pop()
-            tmp = t.pop()
-
-            var sec = ify({}, t.join(':'))
-            sec[tmp] = milli
-            tmp = t.pop()
-
-            var min = ify({}, t.join(':'));
-            min[tmp] = sec;
-            tmp = t.pop();
-
-            var hour = ify({}, t.join(':'));
-            hour[tmp] = min;
-            tmp = t.pop();
-
-            var day = ify({}, t.join(':'));
-            day[tmp] = hour;
-            tmp = t.pop();
-
-            var month = ify({}, t.join(':'));
-            month[tmp] = day;
-            tmp = t.pop();
-
-            var year = ify({}, t.join(':'));
-            year[tmp] = month;
-            tmp = t.pop();
-
-            var time = ify({}, t.join(':') || 'id');
-            time[tmp] = year;
-
-            var timepoint = time
-            gun.put({ last: Gun.state(), timepoint, soul }, 'timegraph/' + soul)
-            //timeMethods.pending.push(soul)
           }
 
-          if (rData)
-            gun.put.call(gunCtx, rData, cb, as)
-          else
-            gun.put.call(gunCtx, data, cb, as)
+          if (!rSoul)
+            rSoul = soul
 
+          gun.put.call(gunCtx, data, cb, as)
+
+
+          var t = new Date(Gun.state()).toISOString().split(/[\-t\:\.z]/ig)
+          var rid = 'timepoint/' + soul
+          t = [rid].concat(t)
+
+          // Working example of original modified slightly
+          var milli = ify({}, t.join(':'))
+          milli['soul'] = rSoul
+
+          var tmp = t.pop()
+          tmp = t.pop()
+
+          var sec = ify({}, t.join(':'))
+          sec[tmp] = milli
+          tmp = t.pop()
+
+          var min = ify({}, t.join(':'));
+          min[tmp] = sec;
+          tmp = t.pop();
+
+          var hour = ify({}, t.join(':'))
+          hour[tmp] = min
+          tmp = t.pop()
+
+          var day = ify({}, t.join(':'))
+          day[tmp] = hour
+          tmp = t.pop()
+
+          var month = ify({}, t.join(':'))
+          month[tmp] = day
+          tmp = t.pop()
+
+          var year = ify({}, t.join(':'))
+          year[tmp] = month
+          tmp = t.pop()
+
+          var time = ify({}, t.join(':') || 'id')
+          time[tmp] = year
+
+          var timepoint = milli //time
+          gun.put.call(gunCtx, { last: Gun.state(), timepoint, soul }, 'timegraph/' + soul)
         }, true)
-
-        return this
       },
 
-      onceOld: function(cb, opts) {
-        // Filter out timegraph objects
-        /*var proxyCall = gun.once.call(this, opts, function(data, key) {
-          if (key === 'timegraph')
-            return
+      once: function(cb) {
+        var gunCtx = this
 
-          // TODO: Filter startTime/stopTime
-          cb && cb(data, key)
-        })*/
+        gun.once.call(gunCtx, function(data) {
+          if (!data)
+            cb && cb.apply(gunCtx, undefined)
 
-        return gun.once.apply(this, timeMethods)
-        //return nodeProxyPoly(proxyCall, timeMethods)
+          var args = arguments
+          const nodeRef = data._['#']
+          root.get('timegraph/' + nodeRef).once(data => {
+            if (withinDate(data.last, startDate, stopDate))
+              cb && cb.apply(gunCtx, Array.prototype.slice.call(args).concat([data.last]))
+          })
+        })
       },
 
       on: function(cb) {
         var gunCtx = this
 
-        gun.on(function(data) {
+        gun.on.call(gunCtx, function(data) {
           var args = arguments
           const nodeRef = data._['#']
           root.get('timegraph/' + nodeRef).once(data => {
@@ -173,8 +184,10 @@
 
   function withinDate(checkDate, startDate, stopDate) {
     // If startDate and stopDate are provided, check within bounds
+    console.log(checkDate, startDate, stopDate)
+
     if (startDate && stopDate)
-      if (checkDate <= startDate && checkDate >= stopDate)
+      if (checkDate >= startDate && checkDate <= stopDate)
         return true
       else
         return false
@@ -192,6 +205,63 @@
     }
 
     return true
+  }
+
+  function firstNode(data) {
+    // This function takes a timepoint and recurses into it to find a ref to the first node.
+    var isLastNode = false
+    var lowestKey = null
+
+    for (var key of Object.keys(data)) {
+      if (key === '_')
+        continue
+
+      if (key === 'soul') {
+        isLastNode = true
+        continue
+      }
+
+      if (key < lowestKey)
+        lowestKey = key
+
+      // TODO: do until first(count), then break out of for.
+      // Each key should at least contain one.. replace top level root items as we go along.
+
+      // TODO: Stack overflow, Performant highly nested first/last $num items
+      /*
+      {
+        2018: { // year
+          12: { // month
+            28: { // Day
+              13: { someData }, // 24: hour
+              14: { someData }, // 24: hour
+              15: { someData }, // 24: hour
+            },
+            29: { // Day
+              15: { someData }, // 24: hour
+            },
+            30: { // Day
+              15: { someData }, // 24: hour
+            },
+          },
+        },
+
+        2019: { // year
+          1: { // month
+            1: { // Day
+              16: { someData }, // 24-hour
+            },
+          },
+        },
+      }
+      */
+    }
+
+    if (isLastNode)
+      return lowestKeyRef
+
+    // todo: may use a range array for this.
+
   }
 
   // Shim for backward compatibility
