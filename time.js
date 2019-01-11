8c96e296d1bb49f386b2e1b76ede777ccde74222
Levi Roberts <bugs181@gmail.com>
2019-01-11 00:22:40 -0600

    Started Chain API Refactor

--- time.js
+++ time.js
@@ -6,230 +6,222 @@
   if (!Gun)
     throw new Error('TimeGraph is meant to be used with Gun')
 
+  var gun, root
+  var ify = Gun.node.ify
+
+  // Object to store the state of TimeGraph, range, window/buffer, and other stateful things.
+  var timeState = {
+    startDate: null,
+    stopDate: null,
+    enforceData: false,
+
+    graph: {
+      low: 0,
+      high: 0,
+    },
+
+    graphKey: {
+      low: null, // Stores lowest soul
+      high: null, // Stores highest soul
+    },
+
+    range: {
+      low: 0,
+      high: 0,
+
+      //now: 0,
+      //tot: 0, // keep in sync with timePointIndex.length
+    },
+
+    newItemCount: 0,
+    usingRange: false,
+  }
+
+  function gunProxy(node, props) {
+    var nodeProps = {}
+    for (var key of Object.keys(props)) {
+      nodeProps[key] = { value: props[key] }
+    }
+
+    var newNode = Object.create(node, nodeProps)
+    newNode = Object.assign(newNode, node)
+
+    return newNode
+  }
+
+  function methodProxy(gun, method) {
+    return function() {
+      console.log(method, 'proxied')
+      return gunProxy(gun[method].apply(this, arguments), gun.timegraph)
+    }
+  }
+
+
   Gun.chain.timegraph = function(startDateOpt, stopDateOpt) {
     'use strict'
 
-    var gun = this, root = gun.back(-1)
-    var ify = Gun.node.ify
-    var opts = gun && gun._ && gun._.root && gun._.root.opt
-    var enforceData = opts.enforceData
+    gun = this, root = gun.back(-1)
 
-    // TODO: These will likely be moved into timeState obj.
-    var startDate, stopDate
+    var opts = gun && gun._ && gun._.root && gun._.root.opt
+    timeState.enforceData = opts.enforceData
+
     if (startDateOpt && (startDateOpt instanceof Date || typeof startDateOpt === 'number' || typeof startDateOpt === 'string'))
-      startDate = new Date(startDateOpt).getTime()
+      timeState.startDate = new Date(startDateOpt).getTime()
 
     if (stopDateOpt && (stopDateOpt instanceof Date || typeof stopDateOpt === 'number' || typeof stopDateOpt === 'string'))
-      stopDate = new Date(stopDateOpt).getTime()
+      timeState.stopDate = new Date(stopDateOpt).getTime()
 
-    // Object to store the state of window/buffer
-    var timeState = {
-      now: 0,
+    // Generic proxy for re-routing Gun methods.
+    Gun.chain.timegraph.get = methodProxy(gun, 'get')
+    Gun.chain.timegraph.set = methodProxy(gun, 'set')
+    Gun.chain.timegraph.map = methodProxy(gun, 'map')
 
-      range: {
-        low: 0,
-        high: 0,
-      },
+    Gun.chain.timegraph.once = Gun.chain.timegraph.event(gun, 'once')
+    Gun.chain.timegraph.on = Gun.chain.timegraph.event(gun, 'on')
 
-      newItemCount: 0,
+    return gunProxy(gun, Gun.chain.timegraph)
+  }
+
+
+  // Shim for backward compatibility
+  var origTimegraph = (typeof Gun.chain.time !== 'undefined') ? Gun.chain.time : null
+  if (origTimegraph)
+    return console.warn('Warning: Original .time() API detected! Please remove it from your project to use the new API.')
+
+  Gun.chain.time = function(data, a, b) {
+    if (b)
+      console.warn('Warning: Detected that you are still using the old TimeGraph API, we recommend switching to the new API syntax for more features and better stability.')
+
+    return Gun.chain.timegraph.time.apply(this, data, a)
+  }
+
+  Gun.chain.timegraph.time = function(data, cb) {
+    if (data instanceof Function) {
+      cb = data
+      //return chainEvent.call(this, 'on', cb, true)
     }
 
-    var timeMethods = {
+    var proxy = gunProxy(this, Gun.chain.timegraph)
+    return Gun.chain.timegraph.set.call(proxy, data, cb)
+  }
 
-      /* BEGIN UN-NEEDED METHODS */
-      // TODO: find all functions inside nodeProxyPoly and proxy them with timeMethods. We don't need map; filtering is taken care of in .on and .once
-      get: function() {
-        return nodeProxyPoly(gun.get.apply(this, arguments), timeMethods)
-      },
+  Gun.chain.timegraph.put = function(data, cb, as, rSoul) {
+    console.log('put called')
+    var gunCtx = this
 
-      map: function() {
-        return nodeProxyPoly(gun.map.apply(this, arguments), timeMethods)
-      },
+    if (Gun.is(data)) {
+      data.get(function(soul) {
+        if (!soul)
+          return cb && cb({ err: "Timegraph cannot link `undefined`!" })
 
-      set: function() {
-        return nodeProxyPoly(gun.set.apply(this, arguments), timeMethods)
-      },
-      /* END UN-NEEDED METHODS */
-
-      time: function(data, cb) {
-        if (data instanceof Function) {
-          cb = data
-          return chainEvent.call(this, 'on', cb, true)
-        }
-
-        return timeMethods.set.call(this, data, cb)
-      },
-
-      put: function(data, cb, as, rSoul) {
-        var gunCtx = this
-
-        if (Gun.is(data)) {
-          data.get(function(soul) {
-            if (!soul)
-              return cb && cb({ err: "Timegraph cannot link `undefined`!" })
-
-            timeMethods.put.call(gunCtx, Gun.val.link.ify(soul), cb, as, soul)
-          }, true)
-          return gunCtx
-        }
-
-        gun.get(function(soul) {
-          // This fixes odd behavior like .set(), because .set does not have a soul on first .put. Passing soul to gun.put() creates weird behavior, so not a solution.
-          if (!soul)
-            return timeMethods.put.call(gunCtx, gun.put.call(gunCtx, data, null, as), cb)
-
-          gun.put.call(gunCtx, data, cb, as)
-
-          // Last ditch effort to find soul for TimeGraph
-          if (!rSoul)
-            rSoul = (as && as.item && as.item._ && as.item._.soul) || soul
-
-          var t = new Date(Gun.state()).toISOString().split(/[\-t\:\.z]/ig)
-          var rid = 'timepoint/' + soul
-          t = [rid].concat(t)
-
-          // Working example of original modified slightly. Will later work this into a loop.
-          var milli = ify({}, t.join(':'))
-          milli['soul'] = rSoul
-
-          var tmp = t.pop()
-          tmp = t.pop()
-
-          var sec = ify({}, t.join(':'))
-          sec[tmp] = milli
-          tmp = t.pop()
-
-          var min = ify({}, t.join(':'));
-          min[tmp] = sec;
-          tmp = t.pop();
-
-          var hour = ify({}, t.join(':'))
-          hour[tmp] = min
-          tmp = t.pop()
-
-          var day = ify({}, t.join(':'))
-          day[tmp] = hour
-          tmp = t.pop()
-
-          var month = ify({}, t.join(':'))
-          month[tmp] = day
-          tmp = t.pop()
-
-          var year = ify({}, t.join(':'))
-          year[tmp] = month
-          tmp = t.pop()
-
-          var time = ify({}, t.join(':') || 'id')
-          time[tmp] = year
-
-          // TODO: FIXME: Need to fix dangling : on last item.
-
-          var timepoint = time //milli //time
-          root.put.call(root, { last: milli, state: Gun.state(), timepoint, soul }, 'timegraph/' + soul)
-        }, true)
-
-        return gunCtx
-      },
-
-      once: function(cb) {
-        return chainEvent.call(this, 'once', cb)
-      },
-
-      on: function(cb) {
-        return chainEvent.call(this, 'on', cb)
-      },
-
-      off: function() {
-        return chainEvent.call(this, 'off')
-      },
-
-      // Subset of API for filtering
-      range: function(startRange, stopRange) {
-        //timeState.now = Gun.state()
-        //
-        timeState.range.low = startRange
-        timeState.range.high = stopRange
-        //traverse.call(this)
-      },
-
-      first: function(count) {
-      },
-
-      last: function(count) {
-      },
-
-      pause: function() {
-      },
-
-      resume: function() {
-      },
-
-      done: function(cb) {
-        // cb will pass new items count since last Gun emit event, potentially along with timegraph of those items. (Requires array or obj, but may be better in user-land)
-        // cb(timeState.newItemCount). The reason this is a seperate API method and not part of .range, .first, etc is so that it can be used in the future for other things.
-
-      },
-
-      filter: function() {
-      },
-
-      // Transformation API
-      transform(cb) {
-        // Transforms data from a Gun chain before being passed.
-      },
-
-      withinDate() {
-        return withinDate.apply(this, arguments)
-      },
+        gun.timegraph.put.call(gunCtx, Gun.val.link.ify(soul), cb, as, soul)
+      }, true)
+      return gunCtx
     }
 
-    function nodeProxyPoly(node, props) {
-      // NOTE: Alternative method would be to just redeclare gun.chain.apiMethod and proxy those instead.
+    gun.get(function(soul) {
+      // This fixes odd behavior like .set(), because .set does not have a soul on first .put. Passing soul to gun.put() creates weird behavior, so not a solution.
+      if (!soul)
+        return gun.timegraph.put.call(gunCtx, gun.put.call(gunCtx, data, null, as), cb)
 
-      var nodeProps = {}
-      for (var key of Object.keys(props)) {
-        nodeProps[key] = { value: props[key] }
-      }
+      gun.put.call(gunCtx, data, cb, as)
 
-      var newNode = Object.create(node, nodeProps)
-      newNode = Object.assign(newNode, node)
+      // Last ditch effort to find soul for TimeGraph
+      if (!rSoul)
+        rSoul = (as && as.item && as.item._ && as.item._.soul) || soul
 
-      return newNode
-    }
+      var t = new Date(Gun.state()).toISOString().split(/[\-t\:\.z]/ig)
+      var rid = 'timepoint/' + soul
+      t = [rid].concat(t)
 
-    function withinDate(checkDate, startDate, stopDate) {
-      // If startDate and stopDate are provided, check within bounds
-      //console.log(checkDate, startDate, stopDate)
+      // Working example of original modified slightly. Will later work this into a loop.
+      var milliStr = t.join(':')
+      var milliSoul = milliStr.substring(0, milliStr.length - 1)
+      var milli = ify({}, milliSoul)
+      milli.soul = rSoul
 
-      if (startDate && stopDate)
-        if (checkDate >= startDate && checkDate <= stopDate)
-          return true
-        else
-          return false
+      var tmp = t.pop()
+      tmp = t.pop()
 
-      // If startDate only provided
-      if (startDate && startDate > checkDate) {
-        return false
-      }
+      var sec = ify({}, t.join(':'))
+      sec[tmp] = milli
+      tmp = t.pop()
 
-      // if stopDate only provided
-      if (stopDate && stopDate < checkDate) {
-        return false
-      }
+      var min = ify({}, t.join(':'));
+      min[tmp] = sec;
+      tmp = t.pop();
 
-      return true
-    }
+      var hour = ify({}, t.join(':'))
+      hour[tmp] = min
+      tmp = t.pop()
 
-    function chainEvent(event, cb, soulOnly) {
-      // TODO: .on/.once can take multiple paths like .first, .last, .range
+      var day = ify({}, t.join(':'))
+      day[tmp] = hour
+      tmp = t.pop()
+
+      var month = ify({}, t.join(':'))
+      month[tmp] = day
+      tmp = t.pop()
+
+      var year = ify({}, t.join(':'))
+      year[tmp] = month
+      tmp = t.pop()
+
+      var time = ify({}, t.join(':'))
+      time[tmp] = year
+
+      var timepoint = time //milli //time
+      root.put.call(root, { last: milli, state: Gun.state(), timepoint, soul }, 'timegraph/' + soul)
+
+      timeState.graph.high = t
+      timeState.graphKey.high = milliSoul
+      console.log(timeState)
+    }, true)
+
+    return gunCtx
+  }
+
+
+  Gun.chain.timegraph.event = function(gun, event, soulOnly) {
+    return function chainEvent(cb) {
       cb = (cb instanceof Function && cb) || function(){}
 
+      // Called from time.range().$event(), ex: people.time().range().once()
+      /*if (timeState.usingRange) {
+        // TODO: Think about if this is the best place to put this. We may need to get the range for every .range() instead, for existing streams.
+        // Would calling .range() on it again affect anything?? Maybe it will still let us use old events. We have .off() if we need to do other events.
+        gun.get(function(soul) {
+          getRange('timepoint/' + soul, function(range)  {
+            console.log('Finished getting range')
+            if (timeState.range.cb instanceof Function)
+              timeState.range.cb(range)
+          })
+        }, true)
+
+        timeState.range.cb = function() {
+          // TODO: getNearestRange(timeState.range.low, timeState.graph.low) // Do we really need this? Can't we just use traverse and check if between the dateRange()
+          //    The above would require getting every single node, though.. Don't we need that for traverse cache anyway?? We could bail early if it's not in the range... hmmm...
+          //  For example, if range(2019), and nodes from 2018 exist, the .getTime() would be sufficient for that check..
+
+          // TODO: do range logic... like findInRange || traverse..
+          // TODO: Soul only
+          // TODO: For each range.. we need to call .once and .on, for .time() this would give us the whole list.. this is fine to be called every time, because the user can .push() to array
+          // Then after iterating each key we call .done()
+          // TODO: .first and .last NEED to use range, otherwise the data isn't meaningul at all.
+
+          cb.apply(this, arguments)
+        }
+
+        return this
+      }*/
+
+      // Not using range, so detour them to TimeGraph node.
       gun.get(function(soul) {
         if (!soul)
           return cb.call(gun, { err: Gun.log('TimeGraph ran into .on error, please report this!') })
 
         root.get('timegraph/' + soul)[event](function(timegraph) {
-          if (!withinDate(timegraph.state, startDate, stopDate))
+          if (!withinDate(timegraph.state, timeState.startDate, timeState.stopDate))
             return
 
           root.get(timegraph.last['#']).once(function(timepoint, key) {
@@ -245,69 +237,30 @@
 
       return this
     }
-
-    function traverse() {
-      //console.log(timegraph)
-      //var t = new Date(Gun.state()).toISOString().split(/[\-t\:\.z]/ig)
-
-      // var r = gun.__.opt.hooks.all(keys, {from: 'user/', upto: '/', start: "c", end: "f"});
-
-      // TODO: Some idea that takes high num - low num to find middle, and store timepoint refs inebtween a sparse array type thing.
-      // Then build a timeIndex of inbetween values. This would be at most 100 to 500 datapoints, changed with a cacheLevel opt config.
-      // 100 datapoints on average, 500 for medium sized data, etc.
-
-      // .from(myLastVisit).to(whenPageLoaded).map().once()
-
-      root.get(function(timepoint, ev) {
-        console.log(arguments)
-      }, true)
-
-      gun.get(function(soul) {
-        gun.get(function(timepoint, ev) {
-          console.log(arguments)
-        })
-        /*gun.get('timepoint/' + soul).get(function(timepoint, ev) {
-          console.log(arguments)
-        })*/
-      }, true)
-      return this
-
-
-
-      gun.get(function(soul) {
-        root.get('timepoint/' + soul).once(function(timepoint) {
-          console.log(timepoint)
-          for (var key of Object.keys(timepoint)) {
-            if (key === '_')
-              continue
-
-            timeState.now = new Date(key).getTime() //.toISOString()
-            if (withinDate(timeState.now, timeState.range.low, timeState.range.high))
-              console.log('within time')
-            else
-              console.log('not within time')
-          }
-        })
-      }, true)
-    }
-
-    function traverseRemote() {
-
-    }
-
-    return nodeProxyPoly(gun, timeMethods)
   }
 
-  // Shim for backward compatibility
-  var origTimegraph = (typeof Gun.chain.time !== 'undefined') ? Gun.chain.time : null
-  if (origTimegraph)
-    return console.warn('Warning: Original .time() API detected! Please remove it from your project to use the new API.')
 
-  Gun.chain.time = function(data, a, b) {
-    if (b)
-      console.warn('Warning: Detected that you are still using the old TimeGraph API, we recommend switching to the new API syntax for more features and better stability.')
+  function withinDate(checkDate, startDate, stopDate) {
+    // If startDate and stopDate are provided, check within bounds
+    //console.log(checkDate, startDate, stopDate)
 
-    return Gun.chain.timegraph.time.apply(this, data, a)
+    if (startDate && stopDate)
+      if (checkDate >= startDate && checkDate <= stopDate)
+        return true
+      else
+        return false
+
+    // If startDate only provided
+    if (startDate && startDate > checkDate) {
+      return false
+    }
+
+    // if stopDate only provided
+    if (stopDate && stopDate < checkDate) {
+      return false
+    }
+
+    return true
   }
 
 }())
